5fa2a1e7f223d16fb496100d27a473639ea80b40
PONS DE GUIGUES Ludovic <ludovic.pons-de-guigues@etu.univ-amu.fr>
2026-08-26 09:10:10 +0200

    .

--- src/lib/simulation/Organisation.ts
+++ src/lib/simulation/Organisation.ts
@@ -1,3 +1,6 @@
+import neo4j from 'neo4j-driver';
+import { NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD } from '$env/static/private';
+
 import { z } from 'zod';
 
 const MaterialResourceSchema = z.object({
@@ -22,7 +25,55 @@ export const OrganisationSchema = z.object({
 type OrganisationData = z.infer<typeof OrganisationSchema>;
 
 export class Organisation {
-	constructor(public data: OrganisationData) {}
+	constructor(public data: OrganisationData) {
+		this.add_to_DB();
+	}
 
-	add_to_DB() {}
+	async add_to_DB() {
+		const driver = neo4j.driver(NEO4J_URI!, neo4j.auth.basic(NEO4J_USERNAME!, NEO4J_PASSWORD!));
+
+		const session = driver.session();
+
+		try {
+			const result = await session.run(
+				`
+			CREATE (o:Organisation {
+				name: $name,
+				description: $description,
+				type: $type,
+				objective: $objective,
+				human_resource: $human,
+				financial_resource: $financial
+			})
+
+			FOREACH (material IN $material |
+				CREATE (r:MaterialResource {
+					name: material.name,
+					description: material.description,
+					quantity: material.quantity,
+					value: material.value
+				})
+				CREATE (o)-[:HAS_RESOURCE]->(r)
+			)
+
+			RETURN o
+			`,
+				{
+					name: this.data.name,
+					description: this.data.description,
+					type: this.data.type,
+					objective: this.data.objective,
+					human: this.data.resource.human,
+					financial: this.data.resource.financial,
+
+					material: this.data.resource.material
+				}
+			);
+
+			return result.records[0].get('o').properties;
+		} finally {
+			await session.close();
+			await driver.close();
+		}
+	}
 }
